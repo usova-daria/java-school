73ce37308c832b18737c0523bbe21d860eb104cf
daria_usova <ds.usova@inbox.ru>
2021-01-20 17:14:48 +0300

    Add string representation of OrderStatus enum

--- src/main/java/com/javaschool/entity/order/enumeration/OrderStatus.java
+++ src/main/java/com/javaschool/entity/order/enumeration/OrderStatus.java
@@ -1,10 +1,27 @@
 package com.javaschool.entity.order.enumeration;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum OrderStatus {
 
-    AWAITING_PAYMENT,
-    AWAITING_SHIPMENT,
-    SHIPPED,
-    DELIVERED
+    AWAITING_PAYMENT("Awaiting payment"),
+    AWAITING_SHIPMENT("Awaiting shipment"),
+    SHIPPED("Shipped"),
+    DELIVERED("Delivered");
 
+    private String status;
+
+    OrderStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return this.status;
+    }
+
+    @JsonValue
+    public String getStatus() {
+        return status;
+    }
 }
