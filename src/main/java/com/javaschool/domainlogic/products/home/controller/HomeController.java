4dde363a320326605645386dc76e74128c288a9c
daria_usova <ds.usova@inbox.ru>
2021-01-20 18:20:49 +0300

    Add HomeController

---
+++ src/main/java/com/javaschool/domainlogic/products/home/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.javaschool.domainlogic.products.home.controller;
+
+import com.javaschool.domainlogic.products.dto.ProductDto;
+import com.javaschool.domainlogic.products.home.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    @Autowired
+    private ProductService productService;
+
+    @ModelAttribute("products")
+    private List<ProductDto> productDtoList() {
+        return productService.getNotDeletedProductsSortedByCreated(4);
+    }
+
+    @GetMapping
+    public String showHomePage() {
+        return "home";
+    }
+
+}
