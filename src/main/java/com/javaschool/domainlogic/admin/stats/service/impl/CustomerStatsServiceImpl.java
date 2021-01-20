5fcc2490fae7d8078dec6858347d4dde9465d41d
daria_usova <ds.usova@inbox.ru>
2021-01-20 19:49:05 +0300

    Add logging to service classes (/admin/stats)

--- src/main/java/com/javaschool/domainlogic/admin/stats/service/impl/CustomerStatsServiceImpl.java
+++ src/main/java/com/javaschool/domainlogic/admin/stats/service/impl/CustomerStatsServiceImpl.java
@@ -4,13 +4,17 @@ import com.javaschool.dao.api.user.UserRepository;
 import com.javaschool.domainlogic.admin.stats.dto.CustomerData;
 import com.javaschool.domainlogic.admin.stats.dto.CustomerStats;
 import com.javaschool.domainlogic.admin.stats.service.api.CustomerStatsService;
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.PersistenceException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Log4j
 public class CustomerStatsServiceImpl implements CustomerStatsService {
 
     @Autowired
@@ -19,7 +23,13 @@ public class CustomerStatsServiceImpl implements CustomerStatsService {
     @Override
     @Transactional(readOnly = true)
     public CustomerStats getCustomerStats() {
-        List<CustomerData> bestCustomers = getTop10CustomersByNumberOfOrders();
+        List<CustomerData> bestCustomers;
+        try {
+             bestCustomers = getTop10CustomersByNumberOfOrders();
+        } catch (PersistenceException e) {
+            log.error("An error occurred while finding top 10 customers", e);
+            bestCustomers = new ArrayList<>();
+        }
         return new CustomerStats(bestCustomers);
     }
 
