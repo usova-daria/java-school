568d3fab0257f771d17329eac0324bd66e09cc08
daria_usova <ds.usova@inbox.ru>
2021-02-07 23:18:58 +0300

    Add service classes ("/api/product-stats")

---
+++ vinyl-store/src/main/java/com/javaschool/domainlogic/salesdisplay/service/SalesDisplayService.java
@@ -0,0 +1,9 @@
+package com.javaschool.domainlogic.salesdisplay.service;
+
+import com.javaschool.domainlogic.salesdisplay.dto.ProductStats;
+
+public interface SalesDisplayService {
+
+    ProductStats getProductStats();
+
+}
