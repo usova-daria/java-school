df7dc67d00c19911a7a12b133ff383c9b1c69a7d
daria_usova <ds.usova@inbox.ru>
2021-02-21 21:43:50 +0300

    Update ProductService

--- vinyl-store/src/main/java/com/javaschool/domainlogic/products/home/service/ProductService.java
+++ vinyl-store/src/main/java/com/javaschool/domainlogic/products/home/service/ProductService.java
@@ -1,6 +1,10 @@
 package com.javaschool.domainlogic.products.home.service;
 
+import com.javaschool.dao.impl.product.projection.ProductNamePriceProjection;
+import com.javaschool.dao.impl.product.projection.ProductPriceProjection;
+import com.javaschool.dao.impl.product.projection.ProductUnitsInStoreProjection;
 import com.javaschool.domainlogic.products.common.dto.ProductDto;
+import com.javaschool.dao.impl.product.projection.ProductProjection;
 
 import java.util.List;
 
@@ -11,5 +15,17 @@ public interface ProductService {
     ProductDto getProductById(Long id);
 
     int getUnitsInStoreById(Long id);
+
+    List<ProductPriceProjection> getPriceByProductId(List<Long> idList);
+
+    boolean getDeletedById(Long id);
+
+    List<ProductDto> getProductsByIdList(List<Long> idList);
+
+    List<ProductProjection> getProductProjectionsByIdList(List<Long> idList);
+
+    List<ProductNamePriceProjection> getNameAndPriceByIdList(List<Long> idList);
+
+    List<ProductUnitsInStoreProjection> getUnitsInStoreByIdList(List<Long> idList);
 
 }
--- vinyl-store/src/main/java/com/javaschool/domainlogic/products/home/service/ProductServiceImpl.java
+++ vinyl-store/src/main/java/com/javaschool/domainlogic/products/home/service/ProductServiceImpl.java
@@ -3,12 +3,14 @@ package com.javaschool.domainlogic.products.home.service;
 import com.javaschool.dao.api.product.ProductRepository;
 import com.javaschool.dao.impl.product.projection.ProductNamePriceProjection;
 import com.javaschool.dao.impl.product.projection.ProductPriceProjection;
+import com.javaschool.dao.impl.product.projection.ProductUnitsInStoreProjection;
 import com.javaschool.domainlogic.products.common.dto.ProductDto;
-import com.javaschool.domainlogic.products.common.dto.ProductProjection;
+import com.javaschool.dao.impl.product.projection.ProductProjection;
 import com.javaschool.domainlogic.products.common.mapper.ProductDtoMapper;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 import javax.persistence.PersistenceException;
@@ -17,7 +19,6 @@ import java.util.List;
 
 @Log4j
 @Service
-@Log4j
 public class ProductServiceImpl implements ProductService {
 
     @Autowired
@@ -47,7 +48,76 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public int getUnitsInStoreById(Long id) {
-        return productRepository.findProductUnitsInStoreById(id);
+        try {
+            return productRepository.findProductUnitsInStoreById(id);
+        } catch (PersistenceException e) {
+            log.error("An error occurred while finding number of units in store for product with id=" + id, e);
+            return 0;
+        }
     }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ProductPriceProjection> getPriceByProductId(List<Long> idList) {
+        try {
+            return productRepository.findPriceByProductId(idList);
+        } catch (PersistenceException e) {
+            log.error("An error occurred while finding total price by product id: " + idList, e);
+            return new ArrayList<>();
+        }
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public boolean getDeletedById(Long id) {
+        try {
+            return productRepository.findDeletedById(id);
+        } catch (PersistenceException e) {
+            log.error("An error occurred while finding deleted by id=" + id, e);
+            return true;
+        }
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ProductDto> getProductsByIdList(List<Long> idList) {
+        List<ProductProjection> products = getProductProjectionsByIdList(idList);
+        return productMapper.toDtoList(products);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ProductProjection> getProductProjectionsByIdList(List<Long> idList) {
+        try {
+            return productRepository.findProductProjectionsByIdList(idList);
+        } catch (PersistenceException e) {
+            log.error("An error occurred while finding product projections by id: " + idList, e);
+            return new ArrayList<>();
+        }
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ProductNamePriceProjection> getNameAndPriceByIdList(List<Long> idList) {
+        try {
+            return productRepository.findProductNameAndPriceByIdList(idList);
+        } catch (PersistenceException e) {
+            log.error("An error occurred while finding product name and price by id: " + idList, e);
+            return new ArrayList<>();
+        }
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ProductUnitsInStoreProjection> getUnitsInStoreByIdList(List<Long> idList) {
+        try {
+            return productRepository.findUnitsInStoreByIdList(idList);
+        } catch (PersistenceException e) {
+            log.error("An error occurred while finding product units in store by id: " + idList, e);
+            return new ArrayList<>();
+        }
+    }
+
 }
